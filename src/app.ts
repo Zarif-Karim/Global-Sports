bfe5902f9a6b8a66ba20ad94c41db0e55691bd8a
Zarif Karim <zarifkarim09@gmail.com>
2023-09-30 18:07:51 +1000

    setup koa router and add nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,31 @@
 import Koa from 'koa';
+import Router from 'koa-router';
+
 const app: Koa = new Koa();
+const router = new Router();
 const port = 8000;
 
-app.use(async ctx => {
-	start = new Date();
+app.use(async (ctx, next) => {
+	const start = new Date() as any;
 	await next();
-	const ms = new Date() - start;
+    const end = new Date() as any;
+	const ms = end - start;
 	console.log('%s %s - %s',
 		ctx.method,
 		ctx.url,
 		ms);
 });
 
-app.get('/badminton', async ctx => {
+router.get('/badminton', async ctx => {
 	ctx.body = 'Badminton Page';
 });
 
-app.use(async ctx => {
+router.get('/', async ctx => {
 	ctx.body = 'GLobal Sports';
 });
 
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
 
 app.listen(port, ()=> console.log(`Listining on http://localhost:${port}`));
