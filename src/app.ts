dfd94819fb8052754a4e4ea8ca52c3d88191177a
Zarif Karim <zarifkarim09@gmail.com>
2023-10-12 00:05:05 +1100

    add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV ?? 'local'}`})
 
 const app: Koa = new Koa();
 const router = new Router();
-const port = 8000;
+const port = process.env.PORT ?? 8080;
 
 app.use(async (ctx, next) => {
 	const start = new Date() as any;
@@ -17,7 +20,7 @@ app.use(async (ctx, next) => {
 });
 
 router.get('/badminton', async ctx => {
-	ctx.body = 'Badminton Page';
+	ctx.body = 'Badminton Page!';
 });
 
 router.get('/', async ctx => {
@@ -28,4 +31,4 @@ app
     .use(router.routes())
     .use(router.allowedMethods());
 
-app.listen(port, ()=> console.log(`Listining on http://localhost:${port}`));
+app.listen(port, ()=> console.log(`NODE_ENV=${process.env.NODE_ENV} | Listining on http://localhost:${port}`));
