979a32750e51113beacf7680794fc3d7fc678f5c
Zarif Karim <zarifkarim09@gmail.com>
2023-10-12 22:42:41 +1100

    add sample data

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,36 @@
-import Koa from 'koa';
-import Router from 'koa-router';
-import dotenv from 'dotenv';
+import Koa from "koa";
+import Router from "koa-router";
+import dotenv from "dotenv";
 
-dotenv.config({ path: `.env.${process.env.NODE_ENV ?? 'local'}`})
+dotenv.config({ path: `.env.${process.env.NODE_ENV ?? "local"}` });
 
 const app: Koa = new Koa();
 const router = new Router();
 const port = process.env.PORT ?? 8080;
 
 app.use(async (ctx, next) => {
-	const start = new Date() as any;
-	await next();
-    const end = new Date() as any;
-	const ms = end - start;
-	console.log('%s %s - %s',
-		ctx.method,
-		ctx.url,
-		ms);
+  const start = new Date() as any;
+  await next();
+  const end = new Date() as any;
+  const ms = end - start;
+  console.log("%s %s - %s", ctx.method, ctx.url, ms);
 });
 
-router.get('/badminton', async ctx => {
-	ctx.body = 'Badminton Page';
+import badmintonData from "./data/badminton";
+router.get("/badminton", async (ctx) => {
+  ctx.status = 200;
+  ctx.type = "json";
+  ctx.body = badmintonData;
 });
 
-router.get('/', async ctx => {
-	ctx.body = 'GLobal Sports';
+router.get("/", async (ctx) => {
+  ctx.body = "GLobal Sports";
 });
 
-app
-    .use(router.routes())
-    .use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(port, ()=> console.log(`NODE_ENV=${process.env.NODE_ENV} | Listining on http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(
+    `NODE_ENV=${process.env.NODE_ENV} | Listining on http://localhost:${port}`
+  )
+);
---
+++ src/data/badminton.ts
@@ -0,0 +1,17 @@
+const data = {
+  "sport": "Badminton",
+  "location": "St Marys",
+  "postcode": "2759",
+  "places": [
+    {
+      "name": "Emerton Youth Leasure Center",
+      "address": "55 Copeland Rd, Emerton NSW 2770",
+      "images": [
+        "https://lh4.googleusercontent.com/p/AF1QipMkkH7U5_imd30DR7ygbyQ5iYDNe7ekMEilhrR3=w1320-h1431-p-k-no",
+        "https://lh4.googleusercontent.com/p/AF1QipPCsLZXMbSgYq_VP1bW6e3_X5_Yb1B2men_SYeW=w1320-h1431-p-k-no"
+      ]
+    }
+  ]
+}
+
+export default data;
